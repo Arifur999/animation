b211b99b226f9e35860a2d6796cce491bf43baba
Arifur999 <arr96777@gmail.com>
2025-11-01 21:55:16 +0600

    added marquee section

--- my-react-app/src/Marquee.jsx
+++ my-react-app/src/Marquee.jsx
@@ -1,24 +1,73 @@
-import React from 'react';
+import React from "react";
 
 const Marquee = () => {
-    return (
-        <>
-        <div className='min-h-screen bg-sky-900 flex justify-center items-center'>
-            <h1>coming soon.............................</h1>
-        </div>
-        <div className='min-h-screen bg-green-400 flex justify-center items-center'>
-            <div className='flex'>
+  return (
+    <>
+      <div className="min-h-screen bg-sky-900 flex justify-center items-center">
+        <h1>coming soon.............................</h1>
+      </div>
+      <div className=" bg-green-400 flex gap-20 py-6">
+        <div className="flex gap-4 shrink-0">
+          <h1 className="text-[70px] text-black font-semibold">
+            Tuilding Better Brands
+          </h1>
 
-<h1 className='text-[70px]'>Tuilding Better Brands</h1>
+          <img
+            className="w-28 h-28"
+            src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg"
+            alt=""
+          />
+        </div>
+        <div className="flex gap-4 shrink-0">
+          <h1 className="text-[70px] text-black font-semibold">
+            Tuilding Better Brands
+          </h1>
 
-<img className='w-28 h-28' src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg" alt="" />
-            </div>
+          <img
+            className="w-28 h-28"
+            src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg"
+            alt=""
+          />
         </div>
-        <div className='min-h-screen bg-sky-900 flex justify-center items-center'>
-            <h1>coming soon.............................</h1>
+        <div className="flex gap-4 shrink-0">
+          <h1 className="text-[70px] text-black font-semibold">
+            Tuilding Better Brands
+          </h1>
+
+          <img
+            className="w-28 h-28"
+            src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg"
+            alt=""
+          />
         </div>
-        </>
-    );
+        <div className="flex gap-4 shrink-0">
+          <h1 className="text-[70px] text-black font-semibold">
+            Tuilding Better Brands
+          </h1>
+
+          <img
+            className="w-28 h-28"
+            src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg"
+            alt=""
+          />
+        </div>
+        <div className="flex gap-4 shrink-0">
+          <h1 className="text-[70px] text-black font-semibold">
+            Tuilding Better Brands
+          </h1>
+
+          <img
+            className="w-28 h-28"
+            src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="min-h-screen bg-sky-900 flex justify-center items-center">
+        <h1>coming soon.............................</h1>
+      </div>
+    </>
+  );
 };
 
 export default Marquee;
