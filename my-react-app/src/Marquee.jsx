810b1060db14879bf531258f8ab7c0d9bbe70058
Arifur999 <arr96777@gmail.com>
2025-11-01 21:44:10 +0600

    start marquee work

--- my-react-app/src/Marquee.jsx
+++ my-react-app/src/Marquee.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 
 const Marquee = () => {
     return (
+        <>
         <div className='min-h-screen bg-sky-900 flex justify-center items-center'>
             <h1>coming soon.............................</h1>
         </div>
+        <div className='min-h-screen bg-green-400 flex justify-center items-center'>
+            <div className='flex'>
+
+<h1 className='text-[70px]'>Tuilding Better Brands</h1>
+
+<img className='w-28 h-28' src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg" alt="" />
+            </div>
+        </div>
+        <div className='min-h-screen bg-sky-900 flex justify-center items-center'>
+            <h1>coming soon.............................</h1>
+        </div>
+        </>
     );
 };
 
